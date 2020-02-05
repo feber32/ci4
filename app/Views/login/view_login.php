eb08f7fcfeea97db9f7d71a5b71b27abad76a2ce
feber <feber32@gmail.com>
2020-02-04 19:33:26 -0600

    jquery

--- app/Views/login/view_login.php
+++ app/Views/login/view_login.php
@@ -5,41 +5,40 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="msapplication-tap-highlight" content="no">
-  <meta name="description" content="<?=cnf_description?>">
-  <meta name="keywords" content="<?=cnf_keywords?>">
-  <title><?=cnf_title?></title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <title></title>
 
   <!-- Favicons-->
-  <link rel="icon" href="<?=baseURL_lib?>images/favicon/<?=favicon_png?>" sizes="32x32">
+  <link rel="icon" href="<?=baseURL_lib?>images/favicon/favicon-32x32.png" sizes="32x32">
   <!-- Favicons-->
-  <link rel="apple-touch-icon-precomposed" href="<?=baseURL_lib?>images/favicon/<?=favicon_png?>">
+  <link rel="apple-touch-icon-precomposed" href="<?=baseURL_lib?>images/favicon/apple-touch-icon-152x152.png">
   <!-- For iPhone -->
   <meta name="msapplication-TileColor" content="#00bcd4">
-  <meta name="msapplication-TileImage" content="<?=baseURL_lib?>images/favicon/<?=favicon_png?>">
+  <meta name="msapplication-TileImage" content="<?=baseURL_lib?>images/favicon/mstile-144x144.png">
   <!-- For Windows Phone -->
 
 
   <!-- CORE CSS-->
+
   <link href="<?=baseURL_lib?>css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection">
   <link href="<?=baseURL_lib?>css/style.min.css" type="text/css" rel="stylesheet" media="screen,projection">
-  <!-- Custome CSS-->
-
+    <!-- Custome CSS-->
+    <link href="<?=baseURL_lib?>css/custom/custom.min.css" type="text/css" rel="stylesheet" media="screen,projection">
   <link href="<?=baseURL_lib?>css/layouts/page-center.css" type="text/css" rel="stylesheet" media="screen,projection">
+
   <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
   <link href="<?=baseURL_lib?>js/plugins/prism/prism.css" type="text/css" rel="stylesheet" media="screen,projection">
   <link href="<?=baseURL_lib?>js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
 
-  <link href="<?=baseURL_lib?>fontawesome-5.12/css/all.css" rel="stylesheet"> <!--load all styles -->
-
-
 </head>
 
-<body class=" grey lighten-2">
+<body class="cyan">
   <!-- Start Page Loading -->
   <div id="loader-wrapper">
-       <div class="progress">
-     <div class="indeterminate"></div>
- </div>
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
   </div>
   <!-- End Page Loading -->
 
@@ -50,34 +49,33 @@
       <form class="login-form">
         <div class="row">
           <div class="input-field col s12 center">
-            <img src="<?=baseURL_lib?>images/<?=logo_png?>" alt="" class="circle responsive-img valign profile-image-login">
-            <p class="center login-form-text"><?=cnf_NombreSys?></p>
+            <img src="<?=baseURL_lib?>images/login-logo.png" alt="" class="circle responsive-img valign profile-image-login">
+            <p class="center login-form-text">Material Design Admin Template</p>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-social-person-outline prefix"></i>
-            <input id="username" type="text" autocomplete="on">
+            <input id="username" type="text">
             <label for="username" class="center-align">Username</label>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
-            <i class="mdi-action-lock-outline prefix "></i>
-            <input id="password" type="password" autocomplete="on">
-            <label for="password" class="">Password</label>
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="password" type="password">
+            <label for="password">Password</label>
           </div>
         </div>
 
         <div class="row">
           <div class="input-field col s12">
-
-            <a id="btn_Entrar" shref="<?=baseURL?>public/index.php/login/valida"  class="btn waves-effect waves-light   col s12   grey darken-1"><i class="fas fa-arrow-circle-right"></i> Entrar</a>
+            <a href="<?=baseURL?>public/index.php/login/valida" class="btn waves-effect waves-light col s12">Entrar</a>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s6 m6 l6">
-            <p class="margin medium-small "><a href="<?=baseURL?>public/index.php/user/nuevo">Register Now!</a></p>
+            <p class="margin medium-small"><a href="<?=baseURL?>public/index.php/user/nuevo">Register Now!</a></p>
           </div>
 
         </div>
@@ -93,9 +91,7 @@
     ================================================ -->
 
   <!-- jQuery Library -->
-  <!--<script type="text/javascript" src="<?=baseURL_lib?>js/plugins/jquery-1.11.2.min.js"></script>
-  <script type="text/javascript" src="<?=baseURL_lib?>jquery-3.4.1/jquery.min.js"></script>-->
-  <script type="text/javascript" src="<?=baseURL_lib?>jquery-3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="<?=baseURL_lib?>js/plugins/jquery-1.11.2.min.js"></script>
   <!--materialize js-->
   <script type="text/javascript" src="<?=baseURL_lib?>js/materialize.min.js"></script>
   <!--prism-->
@@ -104,12 +100,9 @@
   <script type="text/javascript" src="<?=baseURL_lib?>js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 
       <!--plugins.js - Some Specific JS codes for Plugin Settings-->
-    <script type="text/javascript" src="<?=baseURL_lib?>js/plugins.min.js"></script>
+    <script type="text/javascript" src="<?=baseURL_lib?>js/plugins.js"></script>
     <!--custom-script.js - Add your own theme custom JS-->
     <script type="text/javascript" src="<?=baseURL_lib?>js/custom-script.js"></script>
-
-
-
 
 </body>
 
